9079d10202f4176cc8b16946365a4cb77ab1b1cc
John Richardson <johnrichardson5152001@gmail.com>
2025-11-04 16:01:50 -0500

    listening on all nwif

--- Server.py
+++ Server.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 ##Creates a new redis connection client
 class RedisService:
     def __init__(self):
-        self.redis_host = f"{os.environ.get('REDIS_HOST', 'redis://localhost')}"
+        self.redis_host = f"{os.environ.get('REDIS_HOST', 'redis://0.0.0.0')}"
 
     async def get_conn(self):
         return await aioredis.from_url(self.redis_host, encoding="utf-8", decode_responses=True)
